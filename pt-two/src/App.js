9e59ea787fde67a1bec76c5087c41e40dbd528b1
manas8127 <contact2manas24@gmail.com>
2022-04-26 00:55:30 +0530

    work

--- pt-two/src/App.js
+++ pt-two/src/App.js
@@ -2,9 +2,11 @@ import loda from './assets/33810.jpg';
 import './App.css';
 import { useState } from 'react';
 
+
 function App()
 {
-  const [name, setName]=useState('Manas')
+  const [showEvents, setShowEvents]=useState(true)
+  
   const [events, setEvents] = useState(
     [
       {title:'raghav is best', id: 1},
@@ -14,12 +16,33 @@ function App()
     ]
   )
 
+ 
+ console.log(showEvents)
 
   const handleClick=(id)=>{
+    setEvents((mumeinlelo)=>{
+      return mumeinlelo.filter((event)=>{
+        return event.id!==id
+      })
+    }
+
+    )
     console.log(id)
   }
+
   return (
-        <div className="App">
+        <div className="App"> <div>
+            <button onClick={()=>setShowEvents(false)}>
+              Hide Event
+            </button>
+          
+          </div>
+          <div>
+            <button onClick={()=>setShowEvents(true)}>
+              Show Event
+            </button>
+          </div>
+         
 
           {
             events.map((event, index) => (
@@ -34,6 +57,61 @@ function App()
       );
 
 }
+
+// function App()
+// {
+//   const [showEvent, setShowEvent]=useState(true)
+  
+//   const [events, setEvents] = useState(
+//     [
+//       {title:'raghav is best', id: 1},
+//       {title:'manas is best', id: 2},
+//       {title:'shubh is not', id: 3},
+
+//     ]
+//   )
+
+ 
+//  console.log(showEvent)
+
+//   const handleClick=(id)=>{
+//     setEvents((mumeinlelo)=>{
+//       return mumeinlelo.filter((event)=>{
+//         return event.id!==id
+//       })
+//     }
+
+//     )
+//     console.log(id)
+//   }
+
+//   return (
+//         <div className="App"> <div>
+//             <button onClick={(event)=>setShowEvent(false)}>
+//               Hide Event
+//             </button>
+          
+//           </div>
+//           <div>
+//             <button onClick={(event)=>setShowEvent(true)}>
+//               Show Event
+//             </button>
+//           </div>
+         
+
+//           {
+//             events.map((event, index) => (
+//                 <div key = {event.id}> 
+//                 <h2>{index} - {event.title}</h2>
+//                 <button onClick={()=> handleClick(event.id)}>Delete</button>
+//                 </div>
+
+//             ))
+//           }
+//         </div>
+//       );
+
+// }
 
 // function App()
 // {
