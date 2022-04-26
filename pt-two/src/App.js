041484b19dc7159331dd65940699b24a30aa7b97
raghavbhrdwaj <raghavbhradwaj2001@gmail.com>
2022-04-26 19:44:09 +0530

    Intro to state and use state done

--- pt-two/src/App.js
+++ pt-two/src/App.js
@@ -2,61 +2,101 @@ import loda from './assets/33810.jpg';
 import './App.css';
 import { useState } from 'react';
 
+function App() {
+  const [showEvents, setShowEvents] = useState(true)
+  const [events, setEvents] = useState([
+    {title: "mario's birthday bash", id: 1},
+    {title: "bowser's live stream", id: 2},
+    {title: "race on moo moo farm", id: 3}
+  ])
 
-function App()
-{
-  const [showEvents, setShowEvents]=useState(true)
-  
-  const [events, setEvents] = useState(
-    [
-      {title:'raghav is best', id: 1},
-      {title:'manas is best', id: 2},
-      {title:'shubh is not', id: 3},
+  console.log(showEvents)
 
-    ]
-  )
-
- 
- console.log(showEvents)
-
-  const handleClick=(id)=>{
-    setEvents((mumeinlelo)=>{
-      return mumeinlelo.filter((event)=>{
-        return event.id!==id
-      })
-    }
-
-    )
-    console.log(id)
+  const handleClick = (id) => {
+    setEvents(prevEvents => {
+      return prevEvents.filter(event => id !== event.id)
+    })
   }
 
   return (
-        <div className="App"> <div>
-            <button onClick={()=>setShowEvents(false)}>
-              Hide Event
-            </button>
+    <div className="App">
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>Hide Events</button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>Show Events</button>
+        </div>
+      )}
+      {showEvents && events.map((event, index) => (
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>delete event</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+// function App()
+// {
+
+
+//   const [showEvents, setShowEvents]=useState(true)
+//   const [events, setEvents] = useState(
+//     [
+//       {title:'raghav is best', id: 1},
+//       {title:'manas is best', id: 2},
+//       {title:'shubh is not', id: 3},
+
+//     ]
+//   )
+
+ 
+//  console.log(showEvents)
+
+//   const handleClick=(id)=>{
+//     setEvents((mumeinlelo)=>{
+//       return mumeinlelo.filter((event)=>{
+//         return event.id!==id
+//       })
+//     }
+
+//     )
+//     // console.log(id)
+//   }
+
+//   return (
+        
+//         <div className="App"> 
+//         <img src = {loda} className="App-logo"/>
+//         <div>
+//             <button onClick={()=>setShowEvents(false)}>
+//               Hide Event
+//             </button>
           
-          </div>
-          <div>
-            <button onClick={()=>setShowEvents(true)}>
-              Show Event
-            </button>
-          </div>
+//           </div>
+//           <div>
+//             <button onClick={()=>setShowEvents(true)}>
+//               Show Event
+//             </button>
+//           </div>
          
 
-          {
-            events.map((event, index) => (
-                <div key = {event.id}> 
-                <h2>{index} - {event.title}</h2>
-                <button onClick={()=> handleClick(event.id)}>Delete</button>
-                </div>
+//           {
+//             events.map((event, index) => (
+//                 <div key = {event.id}> 
+//                 <h2>{index} - {event.title}</h2>
+//                 <button onClick={()=> handleClick(event.id)}>Delete</button>
+//                 </div>
 
-            ))
-          }
-        </div>
-      );
+//             ))
+//           }
+//         </div>
+//       );
 
-}
+// }
 
 // function App()
 // {
